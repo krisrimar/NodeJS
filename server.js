b06ae36cf48b3326726a5b1af5e1da95de488675
krisrimar <rimarkris@gmail.com>
2017-05-11 22:20:07 +0300

    Update file loading logic
    * Update file loading logic
    
    - Change file loading from loading into buffer to streaming. Results in
    faster file loading
    
    * Remove old file buffer code
    
    - Remove old file buffer code as it has been replaced by file stream
    code

--- server.js
+++ server.js
@@ -22,16 +22,16 @@ var server = http.createServer(function(req, res){
   //Check to see if the file exists
   fs.exists(filepath, function (file_exists){
     if(file_exists) {
-      //Read the file and serve
-      fs.readFile(filepath, function(error, content){
-        if(error) {
-          res.writeHead(500);
-          res.end();
-        } else {
-          res.writeHead(200, { "Content-Type" : contentType});
-          res.end(content, 'utf-8');
-        }
+
+      res.writeHead(200, {"Content-Type" : contentType});
+      //Read the file in streaming mode
+      var streamFile = fs.createReadStream(filepath).pipe(res);
+
+      streamFile.on("error", function(){
+        res.writeHead(500);
+        res.end;
       })
+
     } else {
       res.writeHead(404);
       res.end("sorry we could not find the file you requested");
